26a0687203497c29722bf6ae8169a700eb89b24d
Gustavo Vinicius Taveira Lima <gustavo_vinicius_taveira@hotmail.com>
2019-01-22 20:49:44 -0200

    Adicionando entidades cidade e estado

--- src/main/java/com/udemy/cursouml/CursoumlApplication.java
+++ src/main/java/com/udemy/cursouml/CursoumlApplication.java
@@ -1,8 +1,12 @@
 package com.udemy.cursouml;
 
 import com.udemy.cursouml.domain.Categoria;
+import com.udemy.cursouml.domain.Cidade;
+import com.udemy.cursouml.domain.Estado;
 import com.udemy.cursouml.domain.Produto;
 import com.udemy.cursouml.repositories.CategoriaRepository;
+import com.udemy.cursouml.repositories.CidadeRepository;
+import com.udemy.cursouml.repositories.EstadoRepository;
 import com.udemy.cursouml.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -22,6 +26,12 @@ public class CursoumlApplication implements CommandLineRunner {
 
     @Autowired
     private ProdutoRepository produtoRepository;
+
+    @Autowired
+    private CidadeRepository cidadeRepository;
+
+    @Autowired
+    private EstadoRepository estadoRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CursoumlApplication.class, args);
@@ -44,6 +54,19 @@ public class CursoumlApplication implements CommandLineRunner {
 
         categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
         produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+        Estado est1 = new Estado(null, "Minas Gerais");
+        Estado est2 = new Estado(null, "Sao Paulo");
+
+        Cidade c1 = new Cidade(null, "Uberlandia",est1);
+        Cidade c2 = new Cidade(null, "Sao Paulo",est2);
+        Cidade c3 = new Cidade(null, "Campinas",est2);
+
+        est1.getCidades().addAll(Arrays.asList(c1));
+        est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+        estadoRepository.saveAll(Arrays.asList(est1, est2));
+        cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
     }
 }
 
---
+++ src/main/java/com/udemy/cursouml/repositories/EstadoRepository.java
@@ -0,0 +1,14 @@
+package com.udemy.cursouml.repositories;
+
+import com.udemy.cursouml.domain.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Um objeto desse tipo realiza operacoes de acesso a dados referentes ao objeto categoria,
+ * que esta relacionado com a tabela cateogoria no banco de dados h2. O primeiro parametros da classe
+ * JpaRepository indica o objeto em questao e o segundo parametro indica o tipo do atributo identificador.
+ */
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+}
